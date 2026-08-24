70d8b35f9e46ea3991b97a459aaf03d3297364eb
enzodonadon <empdonadon@gmail.com>
2026-08-24 10:01:34 -0300

    continuacao aula_forms com erro

--- conteudos/aula_forms/form.js
+++ conteudos/aula_forms/form.js
@@ -1,10 +1,14 @@
+const exibir_msg = document.getElementById('login-form')
+exibir_msg.addEventListener('click', efetuar_login());
+
 function efetuar_login() {
     const email = document.getElementById('femail').value;
     const senha = document.getElementById('fpassword').value;
 
     if (email == "admin@gmail.com" && senha == "12345") {
-        alert('Bem-vindo User!');
+        const divMensagem = document.getElementById('mensagemBemVindo');
+        divMensagem.style.display = 'block'; // Mostra a div
     } else {
-        alert('Email ou senha errados!')
+        alert('Email ou senha errados!');
     }
 }
